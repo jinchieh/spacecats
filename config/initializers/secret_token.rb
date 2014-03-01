b6f37ed69c06ccb15161f304fc6078b76f4b327a
Goh Jin Chieh <jgoh@wesleyan.edu>
2014-03-01 11:42:06 -0500

    initialize spacecats

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spacecats::Application.config.secret_token = '9328c1118987abaea8cf6f3cea8a56ca15b741ffc22226f8ec07cf743a0e40911cbaec52d4ee73c8b2fa4b4ffa997eb9846486b300f20685c11137741a68b705'
